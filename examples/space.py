d85f5f2e51d70b4c727d2ce1e3254cbb18b38fda
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-05-28 00:07:33 +0300

    Refactor wget commands in hf_space.yml and space.py for consistency and clarity

--- examples/space.py
+++ examples/space.py
@@ -1,7 +1,7 @@
 """
 uv sync
-wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/ryan/medium/en_US-ryan-medium.onnx
 wget https://huggingface.co/thewh1teagle/phonikud-onnx/resolve/main/phonikud-1.0.int8.onnx
+wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/ryan/medium/en_US-ryan-medium.onnx
 wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/ryan/medium/en_US-ryan-medium.onnx.json
 
 uv pip install gradio
