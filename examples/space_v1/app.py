5ce0f6cf76bf38d3e60af7ff164fe163cce07348
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-06-04 01:29:56 +0300

    update

--- examples/space_v1/app.py
+++ examples/space_v1/app.py
@@ -54,4 +54,4 @@ def serve_audio(filename):
     return app.send_static_file(filename)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=7860)
+    app.run(debug=True, host="0.0.0.0", port=7860)
