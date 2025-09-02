8227012af76566942bf35948df790d0fc71a1c7b
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-09-02 07:37:45 +0300

    Update piper.py

--- examples/piper.py
+++ examples/piper.py
@@ -1,7 +1,7 @@
 """
 uv sync
 wget https://huggingface.co/thewh1teagle/phonikud-onnx/resolve/main/phonikud-1.0.int8.onnx -O phonikud-1.0.int8.onnx
-wget https://huggingface.co/thewh1teagle/phonikud-tts-checkpoints/resolve/main/model.onnx -O tts-model.onnx
+wget https://huggingface.co/thewh1teagle/phonikud-tts-checkpoints/resolve/main/shaul.onnx -O tts-model.onnx
 wget https://huggingface.co/thewh1teagle/phonikud-tts-checkpoints/resolve/main/model.config.json -O tts-model.config.json
 uv run examples/piper.py
 """
