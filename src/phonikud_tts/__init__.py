d5c135b7f52c1c5813ff3f9812d9745bbe32a2a6
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2026-01-25 21:55:42 +0200

    nit

--- src/phonikud_tts/__init__.py
+++ src/phonikud_tts/__init__.py
@@ -1,3 +1,4 @@
-from piper_onnx import Piper
-from phonikud import phonemize
-from phonikud_onnx import Phonikud
+from piper_onnx import Piper # noqa: F401
+from phonikud import phonemize # noqa: F401
+from phonikud_onnx import Phonikud # noqa: F401
+from style_onnx import StyleTTS2 # noqa: F401
