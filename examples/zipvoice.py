05f242a978fa2e9d2b9ceedcb085b5084ed0dd74
yakov <61390950+thewh1teagle@users.noreply.github.com>
2026-02-02 06:22:51 +0200

    feat: integrate Phonikud for Hebrew diacritics and phonemization in zipvoice example

--- examples/zipvoice.py
+++ examples/zipvoice.py
@@ -2,12 +2,15 @@
 wget https://github.com/thewh1teagle/zipvoice-onnx/releases/download/model-files-v1.0/prompt_hebrew_male1.wav -O prompt.wav
 wget https://huggingface.co/thewh1teagle/zipvoice-heb/resolve/main/zipvoice-onnx.tar.gz
 tar -xf zipvoice-onnx.tar.gz
+wget https://huggingface.co/thewh1teagle/phonikud-onnx/resolve/main/phonikud-1.0.int8.onnx
 
 uv run --extra zipvoice examples/zipvoice.py
 """
 
 import soundfile as sf
 from zipvoice_onnx import ZipVoice, ZipVoiceOptions
+from phonikud_onnx import Phonikud
+from phonikud import phonemize
 
 # Example usage with zipvoice_distill model
 options = ZipVoiceOptions(
@@ -17,17 +20,28 @@ options = ZipVoiceOptions(
     fm_decoder_int8_path="./zipvoice-onnx/fm_decoder_int8.onnx",
     model_json_path="./zipvoice-onnx/model.json",
     tokens_path="./zipvoice-onnx/tokens.txt",
+    # uncomment to use GPU
+    # onnx_providers=["CUDAExecutionProvider"],
 )
 
 zipvoice = ZipVoice(options)
 
-# Example usage
-ref_wav = "prompt.wav"
-ref_phonemes = "halˈaχti lamakˈolet liknˈot lˈeχem veχalˈav, ubadˈeʁeχ paɡˈaʃti χavˈeʁ jaʃˈan ʃelˈo ʁaʔˈiti haʁbˈe zmˈan."
-target_phonemes = "halˈaχti lamakˈolet liknˈot lˈeχem veχalˈav, ubadˈeʁeχ paɡˈaʃti χavˈeʁ jaʃˈan ʃelˈo ʁaʔˈiti haʁbˈe zmˈan."
+# Phonemizer setup
+nikud_model_path = "phonikud-1.0.int8.onnx"
+phonikud_model = Phonikud(nikud_model_path)
 
-samples, sample_rate = zipvoice.create(ref_wav, ref_phonemes, target_phonemes)
-print(f"Generated audio: {samples.shape} samples at {sample_rate} Hz")
+# Text to phonemes
+text = "שימו לב נוסעים יקרים, הרכבת תכנס לתחנת תל אביב מרכז בעוד מספר דקות, אנא התרחקו מקצה הרציף והמתינו מאחורי הקו הצהוב, תודה."
+vocalized = phonikud_model.add_diacritics(text)
+target_phonemes = phonemize(vocalized)
+
+# Reference audio and phonemes
+ref_wav = "prompt.wav"
+ref_text = "הלכתי למכולת לקנות לחם וחלב, ובדרך פגשתי חבר ישן שלא ראיתי הרבה זמן."
+ref_vocalized = phonikud_model.add_diacritics(ref_text)
+ref_phonemes = phonemize(ref_vocalized)
+
+samples, sample_rate = zipvoice.create(ref_wav, ref_phonemes, target_phonemes, speed=1.2)
 
 sf.write("audio.wav", samples, sample_rate)
 print("Saved to audio.wav")
